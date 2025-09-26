181b06f377a4c89a4d6c0f66b412ff32672a9f2d
Ramit Goolry <ramitgoolry@gmail.com>
2025-09-26 05:52:41 -0700

    general movement and stuff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -66,7 +66,32 @@ keymaps.general = {
       desc = "Toggle Comment",
       expr = true,
     },
+
+    -- Allow moving the cursor through wrapped lines with j, k, <Up> and <Down>
+    -- http://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
+    -- empty mode is same as using <cmd> :map
+    -- also don't use g[j|k] when in operator pending mode, so it doesn't alter d, y or c behaviour
+    ["j"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', desc = "Move down", expr = true },
+    ["k"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', desc = "Move up", expr = true },
+    ["<Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', desc = "Move up", expr = true },
+    ["<Down>"] = {
+      'v:count || mode(1)[0:1] == "no" ? "j" : "gj"',
+      desc = "Move down",
+      expr = true,
+    },
   },
+
+  v = {
+    ["<Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', desc = "Move up", expr = true },
+    ["<Down>"] = {
+      'v:count || mode(1)[0:1] == "no" ? "j" : "gj"',
+      desc = "Move down",
+      expr = true,
+    },
+    ["<"] = { "<gv", desc = "Indent line" },
+    [">"] = { ">gv", desc = "Indent line" },
+  },
+
   x = {
     ["<leader>/"] = {
       function()
@@ -75,18 +100,12 @@ keymaps.general = {
       desc = "Toggle Comment",
       expr = true,
     },
-  },
-}
 
-keymaps.lsp = {
-  n = {
-    ["<leader>rn"] = {
-      function()
-        -- TODO: Rename
-        vim.notify("Rename not implemented yet")
-      end,
-      desc = "Rename",
-    },
+    ["j"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', desc = "Move down", expr = true },
+    ["k"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', desc = "Move up", expr = true },
+    -- Don't copy the replaced text after pasting in visual mode
+    -- https://vim.fandom.com/wiki/Replace_a_word_with_yanked_text#Alternative_mapping_for_paste
+    ["p"] = { 'p:let @+=@0<CR>:let @"=@0<CR>', desc = "Dont copy replaced text", silent = true },
   },
 }
 
@@ -170,6 +189,23 @@ keymaps.snacks = {
         Snacks.picker.lsp_implementations()
       end,
       desc = "LSP Implementations",
+    },
+
+    -- Terminal
+    ["<leader><leader>"] = {
+      function()
+        Snacks.terminal()
+      end,
+      desc = "Terminal",
+    },
+  },
+
+  t = {
+    ["<leader><leader>"] = {
+      function()
+        Snacks.terminal()
+      end,
+      desc = "Terminal",
     },
   },
 }
