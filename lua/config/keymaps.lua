8ca84f2555a86cf00e63604791abcbd67b43c7d5
Ramit Goolry <ramitgoolry@gmail.com>
2025-09-30 03:09:46 -0700

    move keymaps to central location

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -180,7 +180,7 @@ keymaps.snacks = {
     },
 
     -- Terminal
-    ["<leader><leader>"] = {
+    ["<leader><space>"] = {
       function()
         Snacks.terminal()
       end,
@@ -189,7 +189,7 @@ keymaps.snacks = {
   },
 
   t = {
-    ["<leader><leader>"] = {
+    ["<leader><space>"] = {
       function()
         Snacks.terminal()
       end,
@@ -290,6 +290,63 @@ keymaps.dap = {
         require("dap").step_over()
       end,
       desc = "Step Over",
+    },
+  },
+}
+
+keymaps.sidekick = {
+  n = {
+    ["<leader><leader>"] = {
+      function()
+        require("sidekick.cli").toggle({
+          backend = "tmux",
+        })
+      end,
+      desc = "Toggle Sidekick CLI",
+    },
+    ["<leader>aa"] = {
+      function()
+        require("sidekick.cli").toggle({
+          backend = "tmux",
+        })
+      end,
+      desc = "Toggle Sidekick CLI",
+    },
+    ["<leader>ap"] = {
+      function()
+        require("sidekick.cli").select_prompt()
+      end,
+      desc = "Select Sidekick Prompt",
+    },
+    ["<leader>af"] = {
+      function()
+        require("sidekick.cli").focus()
+      end,
+      desc = "Focus Sidekick CLI",
+    },
+    ["<leader>au"] = {
+      function()
+        require("sidekick.nes").update()
+      end,
+      desc = "Update NES Suggestions",
+    },
+    ["<leader>aj"] = {
+      function()
+        require("sidekick.nes").jump()
+      end,
+      desc = "Jump to NES Hunk",
+    },
+    ["<leader>aA"] = {
+      function()
+        require("sidekick.nes").apply()
+      end,
+      desc = "Apply NES Edits",
+    },
+    ["<leader>ac"] = {
+      function()
+        require("sidekick").clear()
+      end,
+      desc = "Clear Sidekick",
     },
   },
 }
