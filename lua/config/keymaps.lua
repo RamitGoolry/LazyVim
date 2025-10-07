b635bc6567dbc7666a06ea42722a791723606523
Ramit Goolry <ramitgoolry@gmail.com>
2025-10-07 12:36:01 -0700

    centralize keymaps and apply kulala maps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -264,7 +264,7 @@ keymaps.lsp = {
       desc = "LSP Previous diagnostic",
     },
 
-    ["<leader>]"] = {
+    ["<leader>l]"] = {
       function()
         ---@diagnostic disable-next-line: deprecated
         vim.diagnostic.goto_next()
@@ -348,6 +348,83 @@ keymaps.sidekick = {
         require("sidekick").clear()
       end,
       desc = "Clear Sidekick",
+    },
+  },
+}
+
+keymaps.kulala = {
+  n = {
+    ["<leader>Rb"] = {
+      function()
+        require("kulala").scratchpad()
+      end,
+      desc = "Open scratchpad",
+    },
+    ["<leader>Rc"] = {
+      function()
+        require("kulala").copy()
+      end,
+      desc = "Copy as cURL",
+    },
+    ["<leader>RC"] = {
+      function()
+        require("kulala").from_curl()
+      end,
+      desc = "Paste from curl",
+    },
+    ["<leader>Rg"] = {
+      function()
+        require("kulala").download_graphql_schema()
+      end,
+      desc = "Download GraphQL schema",
+    },
+    ["<leader>Ri"] = {
+      function()
+        require("kulala").inspect()
+      end,
+      desc = "Inspect current request",
+    },
+    ["<leader>Rn"] = {
+      function()
+        require("kulala").jump_next()
+      end,
+      desc = "Jump to next request",
+    },
+    ["<leader>Rp"] = {
+      function()
+        require("kulala").jump_prev()
+      end,
+      desc = "Jump to previous request",
+    },
+    ["<leader>Rq"] = {
+      function()
+        require("kulala").close()
+      end,
+      desc = "Close window",
+    },
+    ["<leader>Rr"] = {
+      function()
+        require("kulala").replay()
+      end,
+      desc = "Replay the last request",
+    },
+    ["<leader>Rs"] = {
+      function()
+        require("kulala").run()
+      end,
+      desc = "Send the request",
+    },
+    ["<leader>RS"] = {
+      function()
+        require("kulala").show_stats()
+      end,
+      desc = "Show stats",
+    },
+    ["<leader>Rt"] = {
+      function()
+        require("kulala").toggle_view()
+      end,
+      desc = "Toggle headers/body",
     },
   },
 }
