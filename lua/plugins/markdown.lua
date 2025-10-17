c2a4776ed16fbb56194564dd67a778be6bd55fb2
Ramit Goolry <ramitgoolry@gmail.com>
2025-10-16 18:18:57 -0700

    markdown settings

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,14 @@
+return {
+  -- Disable markdownlint from the markdown extra
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = function(_, opts)
+      opts.linters_by_ft = opts.linters_by_ft or {}
+      -- Remove markdownlint from markdown filetypes
+      opts.linters_by_ft.markdown = vim.tbl_filter(function(linter)
+        return linter ~= "markdownlint" and linter ~= "markdownlint-cli2"
+      end, opts.linters_by_ft.markdown or {})
+    end,
+  },
+}
