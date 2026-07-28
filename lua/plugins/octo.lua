a5fde9fa8d62a762c2fc22e52cf0a20c3e75975f
Ramit Goolry <ramitgoolry@gmail.com>
2026-07-28 16:41:02 +0530

    fix(octo): remove incompatible query override

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -6,12 +6,4 @@ return {
       projects_v2 = false,
     },
   },
-  config = function(_, opts)
-    -- Override queries with local patched version (removes deprecated projectCards)
-    package.loaded["octo.gh.queries"] = nil
-    local queries = dofile(vim.fn.stdpath("config") .. "/lua/plugins/octo-nvim/lua/octo/gh/queries.lua")
-    package.preload["octo.gh.queries"] = function() return queries end
-
-    require("octo").setup(opts)
-  end,
 }
